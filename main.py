9f6f54b6076b97a3e3ad3681718ec469583a3819
nitishv <nitishv@umich.edu>
2021-01-16 00:09:16 -0500

    Initialized new py script, will get to work today afternoon

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Nitish Vijai
+# <nitishv@umich.edu>
+# https://github.com/nitishvijai/QuickJobs
+# main.py
+
+# This file will scrape job postings from major job sites (LinkedIn, Glassdoor, and Indeed).
+# Still working on the implementation.
+
+print("Hello World!")
